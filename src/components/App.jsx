c00924e9f365a14915546aac69b208841f9034f2
Barinulka <barinulka@yandex.ru>
2021-11-19 20:48:58 +0400

    Урок 1. Правки

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,12 @@
 import './App.scss';
-import { Message } from './Message';
+import { Message } from './Message/Message';
 
 const message = 'test props';
 
 function App({name}) {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="app">
+      <header className="app-header">
         <h1>App component</h1>
         <Message message={ message } />
       </header>
