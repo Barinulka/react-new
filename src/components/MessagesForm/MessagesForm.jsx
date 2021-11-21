87838c1a49547221389839a29485cfdf98bbc1cc
Barinulka <barinulka@yandex.ru>
2021-11-21 18:00:10 +0400

    Урок 3

--- src/components/MessagesForm/MessagesForm.jsx
+++ src/components/MessagesForm/MessagesForm.jsx
@@ -1,32 +1,32 @@
+import { Button, TextField } from "@material-ui/core";
 import fakerStatic from "faker/locale/ru";
 import { useState } from "react";
 import { AUTHOR } from "../../constants";
 import "./style.scss";
 
 export const MessagesForm = ({ onSend }) => {
+  const [text, setText] = useState("");
 
-    const [text, setText] = useState('');
+  const handleChangeInput = (e) => {
+    setText(e.target.value);
+  };
 
-    const handleChangeInput = (e) => {
-        setText(e.target.value);
-    }
-
-    // По отправке формы создаем сообщение
-    const handleSubmitForm = (e) => {
-        e.preventDefault()
-        onSend({
-          author: AUTHOR.me,
-          message: text,
-          id: fakerStatic.datatype.uuid(),
-        });
-        setText('');
-    }
+  // По отправке формы создаем сообщение
+  const handleSubmitForm = (e) => {
+    e.preventDefault();
+    onSend({
+      author: AUTHOR.me,
+      message: text,
+      id: fakerStatic.datatype.uuid(),
+    });
+    setText("");
+  };
 
   return (
     <div className="form">
       <form onSubmit={handleSubmitForm}>
-        <input type="text" value={text} onChange={handleChangeInput} />
-        <input type="submit" value="Отправить" />
+        <TextField value={text} onChange={handleChangeInput} />
+        <Button type="submit">Отправить</Button>
       </form>
     </div>
   );
